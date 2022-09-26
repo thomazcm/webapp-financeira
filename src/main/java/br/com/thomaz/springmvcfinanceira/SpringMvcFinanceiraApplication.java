73d4ffc05f77a28eb704ceea2236ce8e9d06615b
Thomaz Machado <thomazcm@gmail.com>
2022-09-26 14:56:41 -0300

    First commit

---
+++ src/main/java/br/com/thomaz/springmvcfinanceira/SpringMvcFinanceiraApplication.java
@@ -0,0 +1,13 @@
+package br.com.thomaz.springmvcfinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcFinanceiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thomaz/springmvcfinanceira/SpringMvcFinanceiraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thomaz.springmvcfinanceira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcFinanceiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
